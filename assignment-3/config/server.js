9fcf4341bcee1905b0fd69782412b0ab06995d03
Lewis Saunders <lewis-saunders@live.nmit.ac.nz>
2021-11-10 12:57:16 +1300

    strapi setup

---
+++ assignment-3/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6644ca1dc94502689672222b23453f7d'),
+    },
+  },
+});
---
+++ assignment-3/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8a2a8af9-5833-496b-9be5-ef74a89597f8'
+};
